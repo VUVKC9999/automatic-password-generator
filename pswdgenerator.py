16f679c09ef919f8b421eb2ab5b3bca0c1278d91
VUVKC9999 <101378981+VUVKC9999@users.noreply.github.com>
2022-11-17 20:10:31 +0530

    Create pswdgenerator.py

---
+++ pswdgenerator.py
@@ -0,0 +1,16 @@
+import random
+
+passl = [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 
+        
+        'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 
+        
+        'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 
+        
+        'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '*', ')', '(', '&', '^', '%', '#', '@', '!']
+
+password = ""
+
+for x in range(4):
+        password = password + random.choices(passl)[0]
+
+        print("Your new password :\n", password)
