1784b5079400424123e6e9e5a616890c13277c71
VUVKC9999 <101378981+VUVKC9999@users.noreply.github.com>
2022-11-17 20:13:04 +0530

    Update pswdgenerator.py

--- pswdgenerator.py
+++ pswdgenerator.py
@@ -1,16 +1,18 @@
 import random
 
-passl = [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 
+passl = [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', '^', '%', '#', 
         
         'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 
         
         'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 
         
-        'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '*', ')', '(', '&', '^', '%', '#', '@', '!']
+        'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '*', ')', '(', '&',
+         
+        '>', '<', '?', '/', '.', ',', ']', '}', '[', '{', ';', ':', '@', '"', '!']
 
 password = ""
 
-for x in range(4):
+for x in range(10):
         password = password + random.choices(passl)[0]
 
         print("Your new password :\n", password)
